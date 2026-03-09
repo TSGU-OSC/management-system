c1e1458e29a2082a6a0e3d6a5b8f73b14856a120
CapableCCat <Z2481331250@163.com>
2026-03-09 16:10:20 +0800

    更新404页面&样式

--- project/osc-front/src/views/NotFound/index.vue
+++ project/osc-front/src/views/NotFound/index.vue
@@ -1,67 +1,37 @@
 <template>
-    <div style="text-align: center;">
-
-        <el-empty>
-        </el-empty>
-
-
-           <p> 你似乎来到了无人之境</p>
-
-            <el-button type="primary" @click="goBack">返回首页</el-button>
-
-
-
-
+    <div class="notFound-content" style = "text-align: center;">
+        <el-empty></el-empty>
+        <p> 404 Not Found ! </p>
+        <div class="notFound-buttonGroup">
+            <el-button type = "primary" @click="goBackHome">返回首页</el-button>
+            <el-button @click="goBackPrevious">返回上一页</el-button>
+        </div>
+            
+        <!-- 可以优化（添加）为返回上一页 -->
     </div>
 </template>
 
-
 <script>
 export default {
-    created() {
-        const h = this.$createElement;
+    // created() {
+    //     const h = this.$createElement;
 
-        this.$notify({
-            title: '页面不存在！',
-            message: h('b', { style: 'color: teal' }, '你所找的页面不存在！')
-        })
-    },
+    //     this.$notify({
+    //         title: '页面不存在！',
+    //         message: h('b', { style: 'color: teal' }, '你所找的页面不存在！')
+    //     })
+    // },
     methods: {
-        goBack() {
+        goBackHome() {
             this.$router.push('/home');
+        },
+        goBackPrevious() {
+            this.$router.back();
         }
     }
-
 }
-
-
-
-
-
 </script>
+
 <style>
-.bottom {
-    margin-top: 13px;
-    line-height: 12px;
-}
 
-.button {
-    padding: 0;
-    float: right;
-}
-
-.image {
-    width: 100%;
-    display: block;
-}
-
-.clearfix:before,
-.clearfix:after {
-    display: table;
-    content: "";
-}
-
-.clearfix:after {
-    clear: both
-}
 </style>
